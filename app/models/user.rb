962eb2f82462ff256a7f9d8f36345ea97a565abf
kanimozhi Mani <kani86hrm@gmail.com>
2021-07-22 03:28:26 +0000

    add model validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ActiveRecord::Base
+   
+    has_many :finstagram_posts
+    has_many :comments
+    has_many :likes
+    
+    validates :email, :username, uniqueness: true
+    validates :email, :username, :avatar_url, :password, presence: true
 
 end
